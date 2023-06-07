9411563067ef1ada743df9f527f587c9052f7501
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-06-07 23:32:52 +0200

    Issue#28: Klasse Faction erstellt und als Entität Annotiert Enum Alignment erstellt, müssen wir noch gucken, wie wir das speichern, ob das auch in die Datenbank geht oder nicht. Konstruktoren, Getter und Setter erstellt

---
+++ Software/AL_Systems_DB2/src/main/java/org/example/Enums/Alignment.java
@@ -0,0 +1,27 @@
+package org.example.Enums;
+
+public enum Alignment {
+    ANGMAR,
+    BLUE_MOUNTAINS,
+    DALE,
+    DOL_GULDUR,
+    DORWINION,
+    RANGER_NORTH,
+    DUNLAND,
+    DWARF,
+    FANGORN,
+    GONDOR,
+    GUNDABAD,
+    HALF_TROLL,
+    HIGH_ELF,
+    HOBBIT,
+    URUK_HAI,
+    GALADHRIM,
+    MORDOR,
+    MOREDAIN,
+    NEAR_HARAD,
+    RHUN,
+    ROHAN,
+    TAUREDAIN,
+    WOOD_ELF
+}
