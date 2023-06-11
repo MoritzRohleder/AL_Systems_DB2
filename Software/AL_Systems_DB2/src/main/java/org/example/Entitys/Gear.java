0dc8b9e258819abcfb40ff9355a47ee0d0a9332f
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-06-11 14:39:15 +0200

    Issue#25: Entität Gear erstellt und Getter, Setter und Konstruktoren implementiert

---
+++ Software/AL_Systems_DB2/src/main/java/org/example/Entitys/Gear.java
@@ -0,0 +1,40 @@
+package org.example.Entitys;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Gear {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int durability;
+
+    public Gear() {
+    }
+
+    public Gear(String name, int durability) {
+        this.name = name;
+        this.durability = durability;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDurability() {
+        return durability;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDurability(int durability) {
+        this.durability = durability;
+    }
+}
