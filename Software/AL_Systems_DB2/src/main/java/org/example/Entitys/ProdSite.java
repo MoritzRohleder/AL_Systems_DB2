ea08fc2d80c8b3247721dd6e12efb89cf2e3ec2f
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-06-11 15:02:48 +0200

    Issue#24:
    Entität ProductionSite (ProdSite) erstellt und Stanard Methoden implementiert
    Enums ProductionSiteType und Resource erstellt

---
+++ Software/AL_Systems_DB2/src/main/java/org/example/Entitys/ProdSite.java
@@ -0,0 +1,54 @@
+package org.example.Entitys;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import org.example.Enums.ProductionSideType;
+import org.example.Enums.Resource;
+
+@Entity
+public class ProdSite {
+    @Id
+    @GeneratedValue
+    private int id;
+    private ProductionSideType type;
+    private Resource resource;
+    private int amount;
+
+    public ProdSite() {
+    }
+
+    public ProdSite(ProductionSideType type, Resource resource, int amount) {
+        this.type = type;
+        this.resource = resource;
+        this.amount = amount;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public ProductionSideType getType() {
+        return type;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setType(ProductionSideType type) {
+        this.type = type;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+}
---
+++ Software/AL_Systems_DB2/src/main/java/org/example/Enums/ProductionSideType.java
@@ -0,0 +1,15 @@
+package org.example.Enums;
+
+public enum ProductionSideType {
+    Farm,
+    FishingLodge,
+    Mine,
+    Quarry,
+    ManFleshPit,
+    Slaughterhouse,
+    HuntingLodge,
+    Orchard,
+    LumberCamp,
+    Workshop,
+    PearlFisher
+}
