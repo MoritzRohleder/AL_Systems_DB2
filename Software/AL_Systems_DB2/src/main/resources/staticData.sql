5beb5b68d4bb507ceee98736ce0e943a9143a5c2
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-06-13 14:11:47 +0200

    Issue#67: Installationsskript vorbereitet

---
+++ Software/AL_Systems_DB2/src/main/resources/staticData.sql
@@ -0,0 +1,19 @@
+/*
+ Inserting the Factions into the Database
+ */
+
+/*
+ Inserting the Regions into the Database
+ */
+
+/*
+ Inserting the ProductionSites into the Database
+ */
+
+/*
+ Inserting the SpecialBuilds into the Database
+ */
+
+/*
+ Inserting the Gear into the Database
+ */
