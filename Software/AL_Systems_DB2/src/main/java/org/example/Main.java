781da0536af6a3fdd03fa3d0ef6c1a195467c885
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-06-14 11:36:05 +0200

    Issue#68: EntityManager implementiert

--- Software/AL_Systems_DB2/src/main/java/org/example/Main.java
+++ Software/AL_Systems_DB2/src/main/java/org/example/Main.java
@@ -1,9 +1,13 @@
 package org.example;
 
+import org.example.DBConntection.DBManager;
+import org.example.Entitys.Faction;
+import org.example.Enums.Alignment;
 import org.example.IO.MainMenu;
 
 public class Main {
     public static void main(String[] args) {
-        MainMenu.run();
+        //MainMenu.run();
+        DBManager.persistFaction(new Faction("Rivendell", "2x healing speed. Can build athelas farms. The farms provide 12 athelas per month. Must show some athelas in the farm build.", "#49688b", Alignment.HIGH_ELF));
     }
 }
