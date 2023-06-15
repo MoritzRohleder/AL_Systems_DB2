81537ef89f5f736d4983e439ba634c2296c3d1d8
MoritzRohleder <moroh001@stud.fh-dortmund.de>
2023-06-15 22:53:37 +0200

    Issue#19: Beziehung, RPChar uses Gear implementiert

--- Software/AL_Systems_DB2/src/main/java/org/example/Entitys/Weapon.java
+++ Software/AL_Systems_DB2/src/main/java/org/example/Entitys/Weapon.java
@@ -1,11 +1,19 @@
 package org.example.Entitys;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.ManyToOne;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity(name = "Weapon")
 public class Weapon extends Gear{
     private String type;
     private double dmg;
+
+    @ManyToMany(mappedBy = "weapons")
+    private List<RPChar> rpchars = new ArrayList<>();
 
     public Weapon() {
         super();
